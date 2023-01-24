d70d6c273ef06762892951feb624da2b3f388a8a
MuieGarda1 <suteulorenzo@gmail.com>
2023-01-24 20:06:51 +0100

    modificato file appearance.c

--- appearance.c
+++ appearance.c
@@ -1,5 +1,17 @@
+/**
+	\file appearance.c
+	\author Suteu Lorenzo Mihai
+	\version 1.0
+	\date 24/01/2023
+*/
 #include <ncurses.h>
-
+/**
+	\fn void dinosaur1 (int y, int x)
+	\brief questa funzione fa vedere una fase del dinosauro in movimento
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: genererà una figura del dinosauro in movimento  
+*/
 void dinosaur1(int y, int x) {
 	mvprintw(y-4, x, "          e-e  ");
 	mvprintw(y-3, x, "        /(\\_/)");
@@ -7,7 +19,13 @@ void dinosaur1(int y, int x) {
 	mvprintw(y-1, x, " '-._, )/'");
 	mvprintw(y, x,   "      \\/");
 }
-
+/**
+	\fn void dinosaur2 (int y, int x)
+	\brief questa funzione fa vedere una fase del dinosauro in movimento
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: genererà una figura del dinosauro in movimento
+*/
 void dinosaur2(int y, int x) {
 	mvprintw(y-4, x, "          e-e  ");
 	mvprintw(y-3, x, "        /(\\_/)");
@@ -15,7 +33,13 @@ void dinosaur2(int y, int x) {
 	mvprintw(y-1, x, " '-._, )/'");
 	mvprintw(y, x,   "      /\\");
 }
-
+/**
+	\fn void cactus1 (int y, int x)
+	\brief questa funzione fa vedere il primo cactus
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: genererà una figura del primo cactus
+*/
 void cactus1(int y, int x) {
 	mvprintw(y-5, x, "    _ ");
 	mvprintw(y-4, x, " _ ( )   ");
@@ -24,7 +48,14 @@ void cactus1(int y, int x) {
 	mvprintw(y-1, x, "   |  /'");
 	mvprintw(y, x, "   | |");
 }
-
+/**
+	\fn void cactus2 (int y, int x)
+	\brief questa funzione fa vedere il secondo cactus
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\param int pair: decide il colore
+	\return int: genererà una figura del secondo cactus
+*/
 void cactus2(int y, int x) {
 	init_pair(5,COLOR_GREEN,COLOR_BLACK);
 	attron(COLOR_PAIR(5));
@@ -37,7 +68,13 @@ void cactus2(int y, int x) {
 	mvprintw(y, x, "   | |");
 	attroff(COLOR_PAIR(5));
 }
-
+/**
+	\fn void sun (int y, int x)
+	\brief questa funzione fa vedere il sole
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: genererà una figura del sole
+*/
 void sun(int y, int x) {
 	attron(COLOR_PAIR(6));
 	mvprintw(y-8, x, "      ;   :   ;");
@@ -51,7 +88,13 @@ void sun(int y, int x) {
 	mvprintw(y, x, "      ;   :   ;");
 	attroff(COLOR_PAIR(6));
 }
-
+/**
+	\fn void moon (int y, int x)
+	\brief questa funzione fa vedere la luna
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: genererà una figura dela luna
+*/
 void moon(int y, int x) {
 	attron(COLOR_PAIR(4));
 	mvprintw(y-8, x, "                      ");
@@ -65,7 +108,13 @@ void moon(int y, int x) {
 	mvprintw(y, x, "    ```                 ");
 	attroff(COLOR_PAIR(4));
 }
-
+/**
+	\fn void showLoss (int y, int x)
+	\brief questa funzione fa vedere la scritta della sconfitta
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: genererà una figura della scritta della sconfitta
+*/
 void showLoss(int y, int x) {
 	mvprintw(y-6, x-15, "88");
 	mvprintw(y-5, x-15, "88");
@@ -78,7 +127,13 @@ void showLoss(int y, int x) {
 	mvprintw(y+3, x-15, "Press 'r' to play again!");
 	mvprintw(y+4, x-15, "Or 'q' to exit from game.");
 }
-
+/**
+	\fn void showTrex (int x, int y)
+	\brief questa funzione fa vedere la scritta iniziale del gioco
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: genererà una figura della scritta iniziiale del gioco
+*/
 void showTrex(int x, int y) {
 	mvprintw(x-7, y-25, "  ,d");
 	mvprintw(x-6, y-25, "  88");
@@ -89,7 +144,13 @@ void showTrex(int x, int y) {
 	mvprintw(x-1, y-25, "  \"Y888 88          `\"Ybbd8\"' 8P\'     `Y8");
 }
 
-
+/**
+	\fn void clearCactus1 (int y, int x)
+	\brief questa funzione nasconde il catus quando vine superato 
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: nasconderà la figura del cactus quando verra superato
+*/
 void clearCactus1(int y, int x) {
 	mvprintw(y-6, x+7, "        ");
 	mvprintw(y-5, x+8, "           ");
@@ -108,7 +169,13 @@ void clearCactus1(int y, int x) {
 		mvprintw(y, 0, "    ");
 	}
 }
-
+/**
+	\fn void clearDinosaurDown (int diY, int diX)
+	\brief questa funzione nasconde il dinosauro quando atterra
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: nascondera il dinosauro quando atterra 
+*/
 void cleardinosaurDown(int diY, int diX) {
 	mvprintw(diY+3, diX+10, "              ");
 	mvprintw(diY+4, diX+8, "              ");
@@ -116,7 +183,13 @@ void cleardinosaurDown(int diY, int diX) {
 	mvprintw(diY+6, diX, "                 ");
 	mvprintw(diY+7, diX, "                "); 
 }
-
+/**
+	\fn void clearDinosaurDown (int diY, int diX)
+	\brief questa funzione nasconde il dinosauro quando in aria
+	\param int x: prima coordinata
+	\param int y: seconda coordinata
+	\return int: nascondera il dinosauro quando in aria
+*/
 void cleardinosaurUp(int diY, int diX) {
     mvprintw(diY-7, diX+4, "         ");
 	mvprintw(diY-8, diX+1, "               ");
