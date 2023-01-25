1cc1cb23e4b1a12c6db9a61feed6f197ff8b46de
ErVecchio <cordaro.mat@gmail.com>
2023-01-25 13:13:57 +0100

    Modificato file game.c

--- game.c
+++ game.c
@@ -1,3 +1,14 @@
+
+/**
+	\file game.c
+	\author Matteo Cordaro
+	\versione 1.0
+	\date 25/01/2023
+*/
+
+
+
+
 #include <ncurses.h>
 #include <unistd.h>
 #include <time.h>
@@ -9,6 +20,16 @@
 #include "minunit.h"
 
 // Check if the game is going to be finished in this turn of loop
+
+/**
+	\fn int checkGame(int y, int x, int diY, int diX)
+	\brief Controlla se il gioco finira\' alla fine del ciclo
+	\param int y: prima coordinata
+	\param int x: seconda coordinata
+	\param int diX: distanza sul piano delle X
+	\param int diY: distanza sul piano delle Y
+	\return int: valore che indica la fine del gioco
+*/
 int checkGame(int y, int x, int diY, int diX) {
 	if (diY == y) {
 		if (abs((diX+14)-x) <= 4) {
@@ -19,6 +40,13 @@ int checkGame(int y, int x, int diY, int diX) {
 }
 
 // Make game faster
+
+/**
+	\fn int computeTime(int delayTime)
+	\brief Rende il gioco piu\' veloce modificando il delay
+	\param int delayTime: tempo di delay tra un'azione e l'altra
+	\return int: tempo di delay
+*/
 int computeTime(int delayTime) {
 	if (delayTime >= 250000) {
 		delayTime -= 1000;
@@ -33,6 +61,14 @@ int computeTime(int delayTime) {
 }
 
 // Which dinosaur should be printed
+
+/**
+	\fn void showdinosaur(int diY, int diX)
+	\bief Questa procedura stampa un differente dinosauro in base a determinati parametri
+	\param int diX: distanza sul piano delle X
+	\param int diY: distanza sul piano delle Y
+	\return int: mostrera\' il primo o il secondo dinosauro
+*/
 void showdinosaur(int diY, int diX) {
 	static int counter = 0;
 	if (counter == 0) {
@@ -46,6 +82,14 @@ void showdinosaur(int diY, int diX) {
 }
 
 // Give user the prize
+
+/**
+	\fn int computePrize(int score, int usedPrize)
+	\brief Questa funzione dara\' il punteggio effettuato
+	\param int score: punteggio
+	\param int usedPrize:
+	\return int: punteggio finale
+*/
 int computePrize(int score, int usedPrize) {
 	if (score >= 20 && score <= 40 && usedPrize == 0) {
 		return 1;
@@ -60,6 +104,13 @@ int computePrize(int score, int usedPrize) {
 }
 
 // The main engine!
+
+/**
+	\fn void startEngine(int highScore, struct user firstUser)
+	\brief Questa procedura compone il codice principale del gioco: e\' presente il funzionamento del gioco, il punteggio, i vari colori presenti e l'alternarsi del giorno e della notte.
+	\param int highScore: punteggio massimo
+	\return int: punteggio ottenuto a fine gioco, distanza percorsa, e verra\' visualizzato un detrminato colore
+*/
 void startEngine(int highScore, struct user firstUser) {
     srand(time(NULL));
 	int x, y, diX=5, prize=0, usedPrize=0, score=0, delayTime = 300000
@@ -180,22 +231,40 @@ int tests_run = 0;
 int tests_passed = 0;
 
 // sample test function 1
+/**
+	\fn static char * test_foo()
+	\brief Una semplice funzione che indica degli errori nei test prima dell'inizio del gioco
+*/
 static char * test_foo() {
     mu_assert("error, foo != 7", foo == 7);
 }
 
 // sample test function 2
+/**
+	\fn static char * test_bar()
+	\brief Una semplice funzione che indica degli errori nei test prima dell'inizio del gioco
+*/
 static char * test_bar() {
     mu_assert("error, bar != 5", bar == 5);
 }
 
 // put all tests here
+/**
+	\fn void all_tests()
+	\brief Una procedura che inserisce i test qui
+*/
 void all_tests() {
 	mu_run_test(test_foo);
 	mu_run_test(test_bar);
 }
 
 // call this to run all tests
+/**
+	\fn int run_all_tests(int argc, char **argv)
+	\brief Una funziona che chiama le due funzioni precedenti
+	\param int argc: un parametro che controlla i test
+	\return int: Inidichera\' se i test sono stati passati e quanti ne sono stati eseguiti
+*/
 int run_all_tests(int argc, char **argv) {
 	if(argc<2 || strcmp(argv[1],"-test")!=0) {
 		return -1;
